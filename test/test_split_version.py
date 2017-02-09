c336df5781a8af485009f8a7cbfbd1f66ddb802b
tsnuggs <tsnuggs@cradlepoint.com>
2017-02-09 09:52:27 -0700

    When fetching the version number fromt he router, it should inlude the patch version. For example, rior to this change the version was logged as '6.2' but now it will be logged as '6.6.6'.

--- test/test_split_version.py
+++ test/test_split_version.py
@@ -14,22 +14,21 @@ class TestSplitVersion(unittest.TestCase):
         from cp_lib.split_version import split_version_string
 
         tests = [
-            (None, None, None, None),
-            (None, "3", 3, 0),
-            (None, "3.4", 3, 4),
-            (None, "3.4.7", 3, 4),
-            ("9.65", None, 9, 65),
-            ("9.65", "3", 9, 65),
-            ("9.65", "3.4", 9, 65),
-            ("9.65", "3.4.7", 9, 65),
-            ("9.65.beta", None, 9, 65),
-            ("9.65.B", "3", 9, 65),
-            ("9.65.beta", "3.4", 9, 65),
-            ("9.65.3.9", "3.4.7", 9, 65),
+            (None, None, None, None, None),
+            (None, "3", 3, 0, 0),
+            (None, "3.4", 3, 4, 0),
+            (None, "3.4.7", 3, 4, 7),
+            ("9.65", None, 9, 65, 0),
+            ("9.65", "3", 9, 65, 0),
+            ("9.65", "3.4", 9, 65, 0),
+            ("9.65", "3.4.7", 9, 65, 0),
+            ("9.65.beta", None, 9, 65, "beta"),
+            ("9.65.B", "3", 9, 65, "B"),
+            ("9.65.beta", "3.4", 9, 65, "beta"),
 
             # bad types
-            (9.65, None, TypeError, 65),
-            (9, None, TypeError, 65),
+            (9.65, None, TypeError, 65, 0),
+            (9, None, TypeError, 65, 0),
         ]
 
         for test in tests:
@@ -38,6 +37,7 @@ class TestSplitVersion(unittest.TestCase):
             source_default = test[1]
             expect_major = test[2]
             expect_minor = test[3]
+            expect_patch = test[4]
 
             if expect_major == TypeError:
                 with self.assertRaises(TypeError):
@@ -45,12 +45,13 @@ class TestSplitVersion(unittest.TestCase):
 
             else:
                 if source_default is None:
-                    major, minor = split_version_string(source)
+                    major, minor, patch = split_version_string(source)
                 else:
-                    major, minor = split_version_string(source, source_default)
+                    major, minor, patch = split_version_string(source, source_default)
 
                 self.assertEqual(major, expect_major)
                 self.assertEqual(minor, expect_minor)
+                self.assertEqual(patch, expect_patch)
 
         return
 
@@ -58,21 +59,20 @@ class TestSplitVersion(unittest.TestCase):
         """
         :return:
         """
-        from cp_lib.split_version import split_version_save_to_dict, SETS_NAME_MAJOR, SETS_NAME_MINOR
+        from cp_lib.split_version import split_version_save_to_dict, SETS_NAME_MAJOR, SETS_NAME_MINOR, SETS_NAME_PATCH
 
         tests = [
-            (None, None, None, None),
-            (None, "3", 3, 0),
-            (None, "3.4", 3, 4),
-            (None, "3.4.7", 3, 4),
-            ("9.65", None, 9, 65),
-            ("9.65", "3", 9, 65),
-            ("9.65", "3.4", 9, 65),
-            ("9.65", "3.4.7", 9, 65),
-            ("9.65.beta", None, 9, 65),
-            ("9.65.B", "3", 9, 65),
-            ("9.65.beta", "3.4", 9, 65),
-            ("9.65.3.9", "3.4.7", 9, 65),
+            (None, None, None, None, None),
+            (None, "3", 3, 0, 0),
+            (None, "3.4", 3, 4, 0),
+            (None, "3.4.7", 3, 4, 7),
+            ("9.65", None, 9, 65, 0),
+            ("9.65", "3", 9, 65, 0),
+            ("9.65", "3.4", 9, 65, 0),
+            ("9.65", "3.4.7", 9, 65, 0),
+            ("9.65.beta", None, 9, 65, "beta"),
+            ("9.65.B", "3", 9, 65, "B"),
+            ("9.65.beta", "3.4", 9, 65, "beta"),
         ]
 
         base = dict()
@@ -83,6 +83,7 @@ class TestSplitVersion(unittest.TestCase):
             source_default = test[1]
             expect_major = test[2]
             expect_minor = test[3]
+            expect_patch = test[4]
 
             if SETS_NAME_MAJOR in base:
                 base.pop(SETS_NAME_MAJOR)
@@ -90,8 +91,12 @@ class TestSplitVersion(unittest.TestCase):
             if SETS_NAME_MINOR in base:
                 base.pop(SETS_NAME_MINOR)
 
+            if SETS_NAME_PATCH in base:
+                base.pop(SETS_NAME_PATCH)
+
             self.assertFalse(SETS_NAME_MAJOR in base)
             self.assertFalse(SETS_NAME_MINOR in base)
+            self.assertFalse(SETS_NAME_PATCH in base)
 
             if expect_major == TypeError:
                 with self.assertRaises(TypeError):
@@ -105,6 +110,7 @@ class TestSplitVersion(unittest.TestCase):
 
                 self.assertEqual(base[SETS_NAME_MAJOR], expect_major)
                 self.assertEqual(base[SETS_NAME_MINOR], expect_minor)
+                self.assertEqual(base[SETS_NAME_PATCH], expect_patch)
 
         base = dict()
         base["fw_info"] = dict()
@@ -115,6 +121,7 @@ class TestSplitVersion(unittest.TestCase):
             source_default = test[1]
             expect_major = test[2]
             expect_minor = test[3]
+            expect_patch = test[4]
 
             if SETS_NAME_MAJOR in base["fw_info"]:
                 base["fw_info"].pop(SETS_NAME_MAJOR)
@@ -122,8 +129,12 @@ class TestSplitVersion(unittest.TestCase):
             if SETS_NAME_MINOR in base["fw_info"]:
                 base["fw_info"].pop(SETS_NAME_MINOR)
 
+            if SETS_NAME_PATCH in base["fw_info"]:
+                base["fw_info"].pop(SETS_NAME_PATCH)
+
             self.assertFalse(SETS_NAME_MAJOR in base["fw_info"])
             self.assertFalse(SETS_NAME_MINOR in base["fw_info"])
+            self.assertFalse(SETS_NAME_PATCH in base["fw_info"])
 
             if expect_major == TypeError:
                 with self.assertRaises(TypeError):
@@ -137,6 +148,7 @@ class TestSplitVersion(unittest.TestCase):
 
                 self.assertEqual(base["fw_info"][SETS_NAME_MAJOR], expect_major)
                 self.assertEqual(base["fw_info"][SETS_NAME_MINOR], expect_minor)
+                self.assertEqual(base["fw_info"][SETS_NAME_PATCH], expect_patch)
 
         return
 
@@ -148,17 +160,16 @@ class TestSplitVersion(unittest.TestCase):
 
         tests = [
             (None, None, None),
-            (None, "3", "3.0"),
-            (None, "3.4", "3.4"),
-            (None, "3.4.7", "3.4"),
-            ("9.65", None, "9.65"),
-            ("9.65", "3", "9.65"),
-            ("9.65", "3.4", "9.65"),
-            ("9.65", "3.4.7", "9.65"),
-            ("9.65.beta", None, "9.65"),
-            ("9.65.B", "3", "9.65"),
-            ("9.65.beta", "3.4", "9.65"),
-            ("9.65.3.9", "3.4.7", "9.65"),
+            (None, "3", "3.0.0"),
+            (None, "3.4", "3.4.0"),
+            (None, "3.4.7", "3.4.7"),
+            ("9.65", None, "9.65.0"),
+            ("9.65", "3", "9.65.0"),
+            ("9.65", "3.4", "9.65.0"),
+            ("9.65", "3.4.7", "9.65.0"),
+            ("9.65.beta", None, "9.65.beta"),
+            ("9.65.B", "3", "9.65.B"),
+            ("9.65.beta", "3.4", "9.65.beta"),
         ]
 
         base = dict()
