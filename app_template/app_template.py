38c4cfb1ec16d97b8726d88782679ad11b9abbba
tsnuggs <tsnuggs@cradlepoint.com>
2017-06-30 14:30:46 -0600

    Remove some code that was left in by accident.

--- app_template/app_template.py
+++ app_template/app_template.py
@@ -17,39 +17,7 @@ APP_NAME = "app_template"
 def start_router_app():
     try:
         cs.CSClient().log(APP_NAME, 'start_router_app()')
-        gps_clients = [{
-            "distance_interval_meters": 0,
-            "enabled": True,
-            "interval": 10,
-            "language": "nmea",
-            "name": "test server",
-            "nmea": {
-                "include_id": True,
-                "prepend_id": False,
-                "provide_gga": True,
-                "provide_rmc": True,
-                "provide_vtg": True
-            },
-            "server": {
-                "lan": True,
-                "port": 12345,
-                "wan": True
-            },
-            "stationary_distance_threshold_meters": 20,
-            "stationary_movement_event_threshold_seconds": 0,
-            "stationary_time_interval_seconds": 0,
-            "taip": {
-                "include_cr_lf_enabled": False,
-                "provide_al": True,
-                "provide_cp": True,
-                "provide_id": True,
-                "provide_ln": True,
-                "provide_pv": True,
-                "report_msg_checksum_enabled": True,
-                "vehicle_id_reporting_enabled": True
-            }
-        }]
-        response = cs.CSClient().put('/config/system/gps/connections', gps_clients)
+
     except Exception as e:
         cs.CSClient().log(APP_NAME, 'Something went wrong in start_router_app()! exception: {}'.format(e))
         raise
