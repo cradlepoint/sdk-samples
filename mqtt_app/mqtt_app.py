fa394619062f97b49b5a72bfb0af616ee13fb558
tsnuggs <tsnuggs@cradlepoint.com>
2017-12-12 10:22:52 -0700

    - Add publish multiple examples and did a little cleanup.

--- mqtt_app/mqtt_app.py
+++ mqtt_app/mqtt_app.py
@@ -1,12 +1,13 @@
 """
 An MQTT App example
+Reference: https://www.eclipse.org/paho/clients/python/docs/
 
 This app does the following:
-- The main code is in mqtt_app.py
 - Connects to MQTT test server ‘test.mosquitto.org’
 - Subscribes to topic ‘paho/test/single/gps’
 - Runs a background thread which publishes router gps info to topic ‘paho/test/single/gps’ every 10 secs.
-- Generates a log when the MQTT server sends the published information for topic ‘paho/test/single/gps’ which was subscribed too.
+- Generates a log when the MQTT server sends the published information for topic ‘paho/test/single/gps’
+  which was subscribed to.
 """
 
 # A try/except is wrapped around the imports to catch an
@@ -47,8 +48,13 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    # client.subscribe('$SYS/#')
-    client.subscribe(settings.MQTT_TOPIC_GPS)
+    topics = [(settings.GPS_TOPIC, settings.MQTT_QOS),
+              (settings.MODEM_TEMP_TOPIC, settings.MQTT_QOS),
+              (settings.WAN_CONNECTION_STATE_TOPIC, settings.MQTT_QOS)]
+    try:
+        client.subscribe(topics)
+    except Exception as ex:
+        log.error('Client Subscribe exception. ex={}'.format(ex))
 
 
 # Called when a message has been received on a topic that the client subscribes
@@ -69,7 +75,7 @@ def on_message(client, userdata, msg):
 # This callback is important because even if the publish() call returns success,
 # it does not always mean that the message has been sent.
 def on_publish(client, userdata, mid):
-    log.debug('Publish response: {}'.format(mid))
+    log.debug('Publish response: Message ID={}'.format(mid))
 
 
 # Called when the broker responds to a subscribe request. The mid variable
@@ -77,18 +83,31 @@ def on_publish(client, userdata, mid):
 # The granted_qos variable is a list of integers that give the QoS level the
 # broker has granted for each of the different subscription requests.
 def on_subscribe(client, userdata, mid, granted_qos):
-    log.debug('Subscribe response: mid={}, granted_qos={}'.format(mid, granted_qos))
+    log.debug('Subscribe response: Message ID={}, granted_qos={}'.format(mid, granted_qos))
 
 
-# This function will periodically publish th router GPS information to
-# the MQTT topic and server based on the parameters in the settings file.
-def send_gps():
-    log.debug('Start send_gps()')
+# This function will periodically publish device data to the MQTT Broker
+def publish_thread():
+    log.debug('Start publish_thread()')
     while True:
         try:
-            gps_lastpos = cs.CSClient().get('/status/gps/lastpos').get('data')
-            publish.single(settings.MQTT_TOPIC_GPS, json.dumps(gps_lastpos),
+            gps_lastpos = cs.CSClient().get(settings.GPS_TOPIC).get('data')
+            gps_pos = {'logitude': gps_lastpos.get('longitude'),
+                       'latitude': gps_lastpos.get('latitude')}
+
+            # Single Topic Publish example
+            publish.single(settings.GPS_TOPIC, payload=json.dumps(gps_pos), qos=settings.MQTT_QOS,
                            hostname=settings.MQTT_SERVER, port=settings.MQTT_PORT)
+
+            # Multiple Topics Publish example
+            modem_temp = cs.CSClient().get(settings.MODEM_TEMP_TOPIC).get('data', '')
+            wan_connection_state = cs.CSClient().get(settings.WAN_CONNECTION_STATE_TOPIC).get('data')
+
+            msgs = [(settings.MODEM_TEMP_TOPIC, modem_temp, settings.MQTT_QOS, False),
+                    (settings.WAN_CONNECTION_STATE_TOPIC, wan_connection_state, settings.MQTT_QOS, False)]
+
+            publish.multiple(msgs=msgs, hostname=settings.MQTT_SERVER, port=settings.MQTT_PORT)
+
             time.sleep(10)
         except Exception as ex:
             log.error('Exception in send_gps(). ex: {}'.format(ex))
@@ -99,7 +118,7 @@ def start_mqtt():
     try:
         log.debug('Start MQTT Client')
 
-        mqtt_client = mqtt.Client()
+        mqtt_client = mqtt.Client(client_id=settings.MQTT_CLIENT_ID)
         mqtt_client.on_connect = on_connect
         mqtt_client.on_message = on_message
         mqtt_client.on_publish = on_publish
@@ -125,7 +144,9 @@ def start_router_app():
         mqtt_thread = Thread(target=start_mqtt, args=())
         mqtt_thread.start()
 
-        send_gps()
+        publish_thread()
+
+        time.sleep(36000)
 
     except Exception as ex:
         log.error('Exception during start_router_app()! exception: {}'.format(ex))
--- mqtt_app/settings.py
+++ mqtt_app/settings.py
@@ -1,4 +1,4 @@
-# Common settings used by mqtt_app
+import cs
 
 # Used for logging or anytime the app name is needed
 APP_NAME = 'mqtt_app'
@@ -6,8 +6,15 @@ APP_NAME = 'mqtt_app'
 # MQTT Server settings
 MQTT_SERVER = 'test.mosquitto.org'
 MQTT_PORT = 1883
+MQTT_PORT_TLS = 8883
+MQTT_QOS = 0
 MQTT_USER_NAME = 'anonymous'
 MQTT_PASSWORD = 'anonymous'
 
 # MQTT Topics
-MQTT_TOPIC_GPS = 'paho/test/single/gps'
+GPS_TOPIC = '/status/gps'
+MODEM_TEMP_TOPIC = '/status/system/modem_temperature'
+WAN_CONNECTION_STATE_TOPIC = '/status/wan/connection_state'
+
+# MQTT Client
+MQTT_CLIENT_ID = cs.CSClient().get('/config/system/system_id').get('data', '')
