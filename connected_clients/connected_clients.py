23cc75c9d91701367db62d86b28d0e9a84e5bd7f
tmcolby <tyson.colby@innotech.engineering>
2021-05-25 18:50:31 -0700

    working

--- connected_clients/connected_clients.py
+++ connected_clients/connected_clients.py
@@ -1,26 +1,19 @@
 from time import sleep
 from csclient import EventingCSClient
-import json
 
-SLEEP_INTERVAL = 10
+SCAN_INTERVAL = 300
 
 cp = EventingCSClient('connected_clients')
-while True:
-    #connected_clients = json.loads(cp.get('/status/lan/clients/'))
-    connected_clients = cp.get('/status/lan/clients/')
-    mac_addresses = ''
-    for client in connected_clients:
-        mac_addresses += client['mac'] + ','
-    mac_addresses = mac_addresses.rstrip(',')
-    cp.put('/config/system/desc', mac_addresses)
-    sleep(SLEEP_INTERVAL)
+cp.log("Scanning for connected clients every {}s".format(SCAN_INTERVAL))
 
-#def update_desc(path, value, *args):
-#    connected_clients = json.loads(value)
-#    mac_addresses = ''
-#    for client in connected_clients:
-#        mac_addresses += client['mac'] + ','
-#    mac_addresses.rstrip(',')
-#    cp.put('/config/system/desc', mac_addresses)
-#
-#cp.register('put', '/status/lan/clients', update_desc)
+while True:
+    connected_clients = cp.get('/status/lan/clients/')
+    mac_addresses = []
+    for client in connected_clients:
+        if client['mac'] not in mac_addresses:
+            mac_addresses.append(client['mac'])
+    mac_addresses = ','.join(mac_addresses)
+    cp.log('connected clients: {}'.format(mac_addresses))
+    cp.put('/config/system/desc/', mac_addresses)
+    desc = cp.get('/config/system/desc/')
+    sleep(SCAN_INTERVAL)
