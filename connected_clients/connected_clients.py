bc7d9a39f6133aac8319be535f77657e658d1124
tmcolby <tyson.colby@innotech.engineering>
2021-05-25 20:45:04 -0700

    Bug Fix
    Set empty string for description when no clients are connected.

--- connected_clients/connected_clients.py
+++ connected_clients/connected_clients.py
@@ -1,18 +1,21 @@
 from time import sleep
 from csclient import EventingCSClient
 
-SCAN_INTERVAL = 300
+SCAN_INTERVAL = 10
 
 cp = EventingCSClient('connected_clients')
 cp.log("Scanning for connected clients every {}s".format(SCAN_INTERVAL))
 
 while True:
     connected_clients = cp.get('/status/lan/clients/')
-    mac_addresses = []
-    for client in connected_clients:
-        if client['mac'] not in mac_addresses:
-            mac_addresses.append(client['mac'])
-    mac_addresses = ','.join(mac_addresses)
+    if connected_clients is not None:
+        mac_addresses = []
+        for client in connected_clients:
+            if client['mac'] not in mac_addresses:
+                mac_addresses.append(client['mac'])
+        mac_addresses = ','.join(mac_addresses)
+    else:
+        mac_addresses = ''
     cp.log('connected clients: {}'.format(mac_addresses))
     cp.put('/config/system/desc/', mac_addresses)
     sleep(SCAN_INTERVAL)
