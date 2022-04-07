0d3e79a7fcb6049b0b6b066ee7460b2cfb353fce
amickel <51377202+amickel@users.noreply.github.com>
2022-04-07 13:43:55 -0600

    Update cslient_compare.sh

--- .github/workflows/cslient_compare.sh
+++ .github/workflows/cslient_compare.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-file2="../hello_world/csclient.py"
-for folder in ../*/;
+file2="hello_world/csclient.py"
+for folder in .;
 do
-	if test -f $folder/csclient.py; then
-		file1=$folder/csclient.py
-		cmp "$file1" "$file2" 
-	fi
+        if test -f $folder/csclient.py; then
+                file1=$folder/csclient.py
+                if !(cmp "$file1" "$file2"); then
+                        echo "The csclient.py file in "$folder" is not the correct version"
+                        exit 1
+                fi
+        fi
 done
