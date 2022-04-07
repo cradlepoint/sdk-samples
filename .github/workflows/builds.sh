47cd8ca6cce0d3ad8d47f8cd92829334edea73cd
amickel <51377202+amickel@users.noreply.github.com>
2022-04-07 13:05:24 -0600

    Add files via upload

---
+++ .github/workflows/builds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ..
+declare -a commands=("python3 make.py build" "python3 make.py build all" "python3 make.py clean" "python3 make.py clean all")
+for cmds in "${commands[@]}";
+do
+	echo $cmds
+	result=$($cmds 2>&1 > /dev/null)
+	if [ -n "$result" ]
+	then
+		echo "Error with app: $result"
+		exit 1
+	fi
+done
