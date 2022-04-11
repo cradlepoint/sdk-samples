061574beee4aee2247dfdf7058f29543c8ff105b
amickel <adriannamickel@u.boisestate.edu>
2022-04-11 13:47:41 -0600

    re-add csclient test

---
+++ .github/workflows/csclient_compare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Every app should have the same version of csclient.py. This test verifies a new app or modified app does not have an out of date csclient.py.
+file2="hello_world/csclient.py"
+for folder in ./*/;
+do
+        if test -f $folder/csclient.py; then
+                file1=$folder/csclient.py
+                if !(cmp "$file1" "$file2"); then
+                        echo "The csclient.py file in "$folder" is not the correct version"
+                        exit 1
+                fi
+        fi
+done
