a45f6cbf88d1a01da8a5ba1d3baebcaf840187fc
Alex Terrell <phate999@gmail.com>
2023-07-03 13:33:23 -0600

    Delete Mobile_Site_Survey directory

--- Mobile_Site_Survey/cp.py
+++
@@ -1,6 +0,0 @@
-"""Monkay patch for cp.uptime()"""
-
-import time
-
-def uptime():
-    return time.time()
--- Mobile_Site_Survey/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cppython Mobile_Site_Survey.py &
-./miniserve -p 8001 results/
