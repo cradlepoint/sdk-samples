7bf2521b10b756888fa7412ac489b4ef5116fae8
Alex Terrell <phate999@gmail.com>
2023-08-02 12:30:23 -0600

    Update system_monitor.py

--- system_monitor/system_monitor.py
+++ system_monitor/system_monitor.py
@@ -15,7 +15,28 @@ previous_ethernet = None
 while True:
     status = {}
 
-    # Get Ethernet port states
+    # CPU Utilization
+    try:
+        cpu = cp.get('status/system/cpu')
+        status["cpu"] = round(sum(list(cpu.values())) * 100)
+        if status["cpu"] > cpu_threshold:
+            cp.alert(f'CPU at {status["cpu"]}%')
+    except Exception as e:
+        cp.logger.exception(e)
+
+    # Memory Utilization
+    try:
+        mem = cp.get('status/system/memory')
+        status["memory"] = round((mem["memtotal"] - mem["memavailable"]) / mem["memtotal"] * 100)
+        if status["memory"] > mem_threshold:
+            cp.alert(f'Memory at {status["memory"]}%')
+    except Exception as e:
+        cp.logger.exception(e)
+
+    # Uptime
+    status["uptime"] = round(cp.get('status/system/uptime'))
+
+    # Get Ethernet port states and alert on change
     try:
         ethernet = {}
         ports = {}
@@ -24,11 +45,6 @@ while True:
         for port in ports:
             ethernet[f'{port["port"]}'] = port["link"]
         status["ethernet"] = ethernet
-    except Exception as e:
-        cp.logger.exception(e)
-
-    # Ethernet port state change
-    try:
         if previous_ethernet:
             diff = dict(set(ethernet.items()) ^ set(previous_ethernet.items()))
             if diff:
@@ -39,6 +55,15 @@ while True:
         previous_ethernet = dict(ethernet)
     except Exception as e:
         cp.logger.exception(e)
+
+    # Put status string to asset_id field
+    cp.put('config/system/asset_id', json.dumps(status))
+
+    # Pause for defined interval
+    time.sleep(interval)
+
+    '''
+    Use these blocks of code to monitor various parameters:
 
     # CPU Utilization
     try:
@@ -61,8 +86,24 @@ while True:
     # Uptime
     status["uptime"] = round(cp.get('status/system/uptime'))
 
-    # Put status string to asset_id field
-    cp.put('config/system/asset_id', json.dumps(status))
+    # Get Ethernet port states and alert on change
+    try:
+        ethernet = {}
+        ports = {}
+        while not ports:
+            ports = cp.get('status/ethernet')
+        for port in ports:
+            ethernet[f'{port["port"]}'] = port["link"]
+        status["ethernet"] = ethernet
+        if previous_ethernet:
+            diff = dict(set(ethernet.items()) ^ set(previous_ethernet.items()))
+            if diff:
+                msg = {"type": "ethernet port state change", "ports": {}}
+                for port in diff:
+                    msg["ports"][port] = ethernet[port]
+                cp.alert(msg)
+        previous_ethernet = dict(ethernet)
+    except Exception as e:
+        cp.logger.exception(e)
 
-    # Pause for defined interval
-    time.sleep(interval)
+'''
