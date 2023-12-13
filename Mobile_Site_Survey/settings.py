e9197a9a46acdef9cc0c1cae515152f23ff965e1
Alex Terrell <phate999@gmail.com>
2023-12-13 16:43:00 -0700

    Update settings.py

--- Mobile_Site_Survey/settings.py
+++ Mobile_Site_Survey/settings.py
@@ -5,6 +5,7 @@ settings = {
     "enable_timer": False,
     "min_time": 0,
     "speedtests": True,
+    "dead_reckoning": True,
     "packet_loss": True,
     "write_csv": True,
     "debug": False,
