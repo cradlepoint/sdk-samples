fa26825df5f57b3700f606588c3cbf731fe7a15e
Alex Terrell <phate999@gmail.com>
2024-04-25 10:42:50 -0600

    Update ipverify_custom_action.py

--- ipverify_custom_action/ipverify_custom_action.py
+++ ipverify_custom_action/ipverify_custom_action.py
@@ -1,13 +1,14 @@
 # ipverify_custom_action - register function for callback on ipverify status change
+# This example would use ipverify to ping a host over a VPN and the app will restart
+# the VPN service if the ipverify test fails.
+# This assumes the first ipverify test is the one we want to monitor.
+#
 # Log examples:
 # 11:58:41 AM INFO ipverify_custom_action VPN Monitor Failed - Resetting Tunnel.
-# 11:58:54 AM INFO ipverify_custom_action VPN Recovered.
-# ipverify_uid is default uid for first ipverify test - modify as needed to match your test
+
 
 from csclient import EventingCSClient
 import time
-
-ipverify_uid = '00000000-91a5-3a5b-ac9b-5ae6367d2d59'
 
 
 def custom_action(path, value, *args):
@@ -20,5 +21,6 @@ def custom_action(path, value, *args):
 
 cp = EventingCSClient('ipverify_custom_action')
 cp.log('Starting...')
+ipverify_uid = cp.get('config/identities/ipverify/0/_id_')
 cp.on('put', f'status/ipverify/{ipverify_uid}/pass', custom_action)
 time.sleep(999999)
