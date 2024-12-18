21003d130140d3d9731def762628d67ecaa55fed
Alex Terrell <phate999@gmail.com>
2024-12-18 07:46:03 -0700

    Update gpio_any_wan_connected.py
    added state tracking and logging

--- gpio_any_wan_connected/gpio_any_wan_connected.py
+++ gpio_any_wan_connected/gpio_any_wan_connected.py
@@ -3,7 +3,11 @@ from csclient import EventingCSClient
 import time
 cp = EventingCSClient('gpio_any_wan_connected')
 cp.log('Starting...')
+previous_state = None
 while True:
     state = int(cp.get('status/wan/connection_state') == 'connected')
-    cp.put('control/gpio/CONNECTOR_GPIO_1', state)
+    if state != previous_state:
+        cp.put('control/gpio/ACCESSORY_GPIO_1', state)
+        cp.log(f'Set control/gpio/ACCESSORY_GPIO_1 to {state}')
+        previous_state = state
     time.sleep(1)
