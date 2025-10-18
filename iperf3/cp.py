4507bc9b39152ba49e11c82bf7f9da3fdaf2728c
GitHub Action <action@github.com>
2025-10-18 14:56:43 +0000

    Auto-update cp.py files across all samples

--- iperf3/cp.py
+++ iperf3/cp.py
@@ -4767,7 +4767,7 @@ def get_appdata(name: str = '') -> Union[Optional[str], Optional[List[Dict[str, 
         appdata = _cs_client.get('config/system/sdk/appdata')
         if not name:
             return appdata
-        return next((x["value"] for x in appdata if x["name"] == name), None)
+        return next((x["value"] for x in appdata if x["name"].lower() == name.lower()), None)
     except Exception as e:
         log("Error getting appdata for {name}: {e}")
         return None
